a573f90b817d4f68f17acab4d0cc3c6567acf46d
oroboros <prashant_401@hotmail.com>
2025-08-14 18:39:57 +0530

    Add files via upload

---
+++ datacallector_gcp/src/main/java/com/example/datacallector/DatacallectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacallector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacallectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacallectorApplication.class, args);
+	}
+
+}
